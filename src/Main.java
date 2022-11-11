689eaa5bde0d91f5fab93c9e3aa56187d477f516
Squesk <filofilip12@protonmail.com>
2022-11-11 20:04:14 +0100

    Add new files

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=25; i++)
+        {
+            System.out.println(i + " " + is_prime(i));
+        }
+
+    }
+
+    public static boolean is_prime(int number)
+    {
+        if(number < 3)
+        {
+            return false;
+        }
+        for(int i=2; i<number; i++)
+        {
+            if(number%i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
